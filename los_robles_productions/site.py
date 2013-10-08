4816feed803719cebb53b5757fcee325168dc5a1
Conrad Weidenkeller <conrad@weidenkeller.com>
2013-10-08 10:36:10 +0000

    Moved static content to cloud files

--- los_robles_productions/site.py
+++ los_robles_productions/site.py
@@ -1,33 +1,47 @@
 from flask import Flask, render_template, request
 from gevent.wsgi import WSGIServer
 app = Flask(__name__)
-
+CF_URL = 'http://bd1bd20de0e06484ccd2-a4b933abf0480693b1cbd92bb7f0259c' + \
+         '.r92.cf1.rackcdn.com/'
+BOOTSTRAP_THEME = 'bootstrap-theme.min.css'
+BOOTSTRAP_CSS = 'bootstrap.min.css'
+BOOTSTRAP = 'bootstrap.min.js'
+CUSTOM_CSS = 'custom.css'
+JQUERY = 'jquery.min.js'
+def _render(template_name):
+    return render_template(template_name,
+                           cf_url=CF_URL,
+                           bootstrap_theme=BOOTSTRAP_THEME,
+                           bootstrap_css=BOOTSTRAP_CSS,
+                           bootstrap=BOOTSTRAP,
+                           custom_css=CUSTOM_CSS,
+                           jquery=JQUERY)
+                                          
 @app.route("/")
 def home():
     if not request.args.get('l'): 
-        return render_template('home.html')
+        return _render('home.html')
     else:
-        return render_template('es_home.html')
+        return _render('es_home.html')
 @app.route('/our_story')
 def about_us():
     if not request.args.get('l'):
-        return render_template('our_story.html')
+        return _render('our_story.html')
     else:
-        return render_template('es_our_story.html')
+        return _render('es_our_story.html')
 @app.route('/contact_us')
 def contact_us():
     if not request.args.get('l'):
-        return render_template('contact_us.html')
+        return _render('contact_us.html')
     else:
-        return render_template('es_contact_us.html')
+        return _render('es_contact_us.html')
 @app.route('/book/<name>')
 def book(name):
     if not request.args.get('l'):
-        return render_template('book.html')
+        return _render('book.html')
     else:
-        return render_template('es_book.html')
+        return _render('es_book.html')
 
 if __name__ == "__main__":
-    #app.run(port=80, host='0.0.0.0', debug=True)
     ws = WSGIServer(('', 80), app)
     ws.serve_forever()
